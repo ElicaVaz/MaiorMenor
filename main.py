c88f297cb9d027157f164743dc452a49ce74be71
ElicaVaz <99455147+ElicaVaz@users.noreply.github.com>
2022-02-18 15:02:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+N1 = int(input('Digite um número inteiro: '))
+N2 = int(input('Digite outro número inteiro: '))
+N3 = int(input('Digite um terceiro número inteiro: '))
+
+x = max(N1,N2,N3)
+y = min(N1,N2,N3)
+
+print('O maior número digitado foi {} e o menor número digitado foi {}.' .format(x,y))
